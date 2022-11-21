daefbea00904ed09a1e377c9870761a263dae6a8
valegrei <victoralegre@uni.pe>
2022-11-20 23:52:25 -0500

    avance de jwt 2

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import HttpStatus from './util/http.status.js';
 import authRoutes from './route/auth.routes.js';
 import usuarioRoutes from './route/usuario.route.js';
 import logger from './util/logger.js';
+import { expressjwt } from "express-jwt";
 
 dotenv.config();
 const PORT = process.env.SERVER_PORT || 3000;
@@ -14,9 +15,27 @@ const app = express();
 app.use(cors({origin: '*'}));
 app.use(express.json());
 
+//Jwt middleware
+app.use('/api',
+    expressjwt({
+      secret: process.env.TOKEN_SECRET,
+      algorithms: ["HS256"],
+      issuer: 'http://carwashperuapp.com/ws',
+      audience: 'htpp://carwashperuapp.com/app'
+    })
+);
+//demas reglas
 app.use('/auth', authRoutes);
-app.use('/usuarios', usuarioRoutes);
+app.use('/api/usuarios', usuarioRoutes);
 app.get('/', (req, res) => res.send(new Response(HttpStatus.OK.code, HttpStatus.OK.status, 'Carwash API, v1.0.0 - All Systems Go')));
-app.get('*', (req, res) => res.status(HttpStatus.NOT_FOUND.code)
+app.use('*', (req, res) => res.status(HttpStatus.NOT_FOUND.code)
     .send(new Response(HttpStatus.NOT_FOUND.code, HttpStatus.NOT_FOUND.status, 'La ruta no existe')));
+app.use(function (err, req, res, next) {    //Manejar error de token invalido
+    if (err.name === "UnauthorizedError") {
+        res.status(HttpStatus.UNAUTHORIZED.code)
+        .send(new Response(HttpStatus.UNAUTHORIZED.code, HttpStatus.UNAUTHORIZED.status, 'Token invalido'));
+    } else {
+        next(err);
+    }
+});
 app.listen(PORT, () => logger.info(`Server runing on: ${ip.address()}:${PORT}`));
--- src/util/http.status.js
+++ src/util/http.status.js
@@ -3,6 +3,7 @@ const HttpStatus = {
     CREATED: { code: 201, status: 'CREATED'},
     NO_CONTENT: { code: 204, status: 'NO_CONTENT'},
     BAD_REQUEST: { code: 400, status: 'BAD_REQUEST'},
+    UNAUTHORIZED: {code: 401, status: 'UNAUTHORIZED'},
     NOT_FOUND: { code: 404, status: 'NOT_FOUND'},
     INTERNAL_SERVER_ERROR: { code: 500, status: 'INTERNAL_SERVER_ERROR'}
 };
